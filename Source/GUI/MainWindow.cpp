0b2aea13fb5c12363376247e50dc326383bf40c1
aldelaro5 <aldelaro5@gmail.com>
2019-04-11 03:28:37 -0400

    Bump the version to beta 0.5

--- Source/GUI/MainWindow.cpp
+++ Source/GUI/MainWindow.cpp
@@ -349,7 +349,7 @@ void MainWindow::onAbout()
 {
   QString title = tr("About Dolphin memory engine");
   QString text =
-      "Beta version 0.4\n\n" +
+      "Beta version 0.5\n\n" +
       tr("A RAM search made to facilitate research and reverse engineering of GameCube and Wii "
          "games using the Dolphin emulator.\n\nThis program is licensed under the MIT license. You "
          "should have received a copy of the MIT license along with this program");
