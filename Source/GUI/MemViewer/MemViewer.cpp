f89d45234c4c0e4cdae4ce80b26ca25ce0ba0255
campital <33563097+campital@users.noreply.github.com>
2024-01-16 16:04:22 -0600

    Fix macOS monospace

--- Source/GUI/MemViewer/MemViewer.cpp
+++ Source/GUI/MemViewer/MemViewer.cpp
@@ -15,6 +15,7 @@
 #include <QPainter>
 #include <QRegularExpression>
 #include <QScrollBar>
+#include <QFontDatabase>
 
 #include "../../Common/CommonUtils.h"
 #include "../../DolphinProcess/DolphinAccessor.h"
@@ -372,6 +373,10 @@ void MemViewer::updateFontSize(int newSize)
   setFont(QFont("Monospace", m_memoryFontSize));
 #elif _WIN32
   setFont(QFont("Courier New", m_memoryFontSize));
+#elif __APPLE__
+  QFont fixedFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
+  fixedFont.setPointSize(m_memoryFontSize);
+  setFont(fixedFont);
 #endif
 
   m_charWidthEm = fontMetrics().horizontalAdvance(QLatin1Char('M'));
