0b4a99f979865c41140d8e1d6feb8c3ad514f117
cristian64 <cristian64@gmail.com>
2024-03-06 19:24:11 +0000

    Add command-line parser.
    It enables the use of `--version` and `--help` in the command line.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QCommandLineParser>
 
 #include "GUI/MainWindow.h"
 
@@ -7,6 +8,14 @@ int main(int argc, char** argv)
   QApplication app(argc, argv);
   QApplication::setApplicationName("Dolphin Memory Engine");
   QApplication::setApplicationVersion("0.9.0");
+
+  QCommandLineParser parser;
+  parser.setApplicationDescription(
+      QObject::tr("A RAM search made specifically to search, monitor and edit "
+                  "the Dolphin Emulator's emulated memory."));
+  parser.addHelpOption();
+  parser.addVersionOption();
+  parser.process(app);
 
   MainWindow window;
   window.show();
