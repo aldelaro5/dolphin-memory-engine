5c91565b2477b3f2de5aeb1986bc172bbc9ad576
cristian64 <cristian64@gmail.com>
2024-03-06 19:16:58 +0000

    Set application name and application version in Qt.

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
+  QApplication::setApplicationName("Dolphin Memory Engine");
+  QApplication::setApplicationVersion("0.9.0");
+
   MainWindow window;
   window.show();
   return app.exec();
