df2ad5bc72bafbb5348db38abe9b77b7b7ec5148
dreamsyntax <dreamsyntax@gmail.com>
2024-04-30 17:00:33 -0700

    set version 1.1.0 for release

--- Source/main.cpp
+++ Source/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
   QApplication::setApplicationName("Dolphin Memory Engine");
-  QApplication::setApplicationVersion("1.0.0");
+  QApplication::setApplicationVersion("1.1.0");
 
   QCommandLineParser parser;
   parser.setApplicationDescription(
