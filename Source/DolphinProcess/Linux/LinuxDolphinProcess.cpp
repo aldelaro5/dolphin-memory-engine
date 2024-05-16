aldelaro5/dolphin-memory-engine

commit d58cd7687e272a576e49c32a91355000184155a4

Address `readability-simplify-boolean-expr` warnings.
The warnings were:

```
/w/dolphin-memory-engine/Source/DolphinProcess/Linux/LinuxDolphinProcess.cpp:136:12: warning: redundant boolean literal in conditional return statement [readability-simplify-boolean-expr]
  136 |     return false;
      |            ^
/w/dolphin-memory-engine/Source/DolphinProcess/Linux/LinuxDolphinProcess.cpp:271:12: warning: redundant boolean literal in conditional return statement [readability-simplify-boolean-expr]
  270 |   if (nwrote != size)
      |   ~~~~~~~~~~~~~~~~~~~
      |   return nwrote == size
  271 |     return false;
      |     ~~~~~~~^~~~~~
  272 |
  273 |   return true;
      |   ~~~~~~~~~~~
/w/dolphin-memory-engine/Source/GUI/MemViewer/MemViewer.cpp:763:9: warning: boolean expression can be simplified by DeMorgan's theorem [readability-simplify-boolean-expr]
  763 |     if (!(value >= '0' && value <= '9') && !(value >= 'A' && value <= 'F'))
      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      |                 <      ||       >
/w/dolphin-memory-engine/Source/GUI/MemViewer/MemViewer.cpp:763:44: warning: boolean expression can be simplified by DeMorgan's theorem [readability-simplify-boolean-expr]
  763 |     if (!(value >= '0' && value <= '9') && !(value >= 'A' && value <= 'F'))
      |                                            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      |                                                    <      ||       >
/w/dolphin-memory-engine/Source/GUI/MemViewer/MemViewer.cpp:983:7: warning: boolean expression can be simplified by DeMorgan's theorem [readability-simplify-boolean-expr]
  983 |   if (!(m_currentFirstAddress + (m_numColumns * rowIndex + columnIndex) >= m_memViewStart &&
      |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      |                                                                         <                 ||

Source/DolphinProcess/Linux/LinuxDolphinProcess.cpp
@@ -131,10 +131,8 @@ bool LinuxDolphinProcess::findPID()
   }
   closedir(directoryPointer);
 
-  if (m_PID == -1)
-    // Here, Dolphin apparently isn't running on the system
-    return false;
-  return true;
+  const bool running{m_PID != -1};
+  return running;
 }
 
 bool LinuxDolphinProcess::readFromRAM(const u32 offset, char* buffer, const size_t size,
@@ -208,7 +206,7 @@ bool LinuxDolphinProcess::writeToRAM(const u32 offset, const char* buffer, const
 {
   struct iovec local;
   struct iovec remote;
-  size_t nwrote;
+
   u64 RAMAddress = 0;
   if (m_ARAMAccessible)
   {
@@ -264,13 +262,16 @@ bool LinuxDolphinProcess::writeToRAM(const u32 offset, const char* buffer, const
     }
     }
   }
-
-  nwrote = process_vm_writev(m_PID, &local, 1, &remote, 1, 0);
   delete[] bufferCopy;
-  if (nwrote != size)
-    return false;
 
-  return true;
+  const ssize_t nwrote{process_vm_writev(m_PID, &local, 1, &remote, 1, 0)};
+  if (nwrote == -1)
+  {
+    // A more specific error type should be available in `errno` (if ever interested).
+    return false;
+  }
+
+  return static_cast<size_t>(nwrote) == size;
 }
 }  // namespace DolphinComm
 #endif

Source/DolphinProcess/Mac/MacDolphinProcess.cpp
@@ -38,9 +38,8 @@ bool MacDolphinProcess::findPID()
     }
   }
 
-  if (m_PID == -1)
-    return false;
-  return true;
+  const bool running{m_PID != -1};
+  return running;
 }
 
 bool MacDolphinProcess::obtainEmuRAMInformations()

Source/GUI/MemViewer/MemViewer.cpp
@@ -760,7 +760,8 @@ void MemViewer::keyPressEvent(QKeyEvent* event)
     // Beep when entering a non-valid value
     const std::string hexChar = event->text().toUpper().toStdString();
     const char value = hexChar[0];
-    if (!(value >= '0' && value <= '9') && !(value >= 'A' && value <= 'F'))
+    const bool ishex{('0' <= value && value <= '9') || ('A' <= value && value <= 'F')};
+    if (!ishex)
     {
       QApplication::beep();
       return;
@@ -980,8 +981,10 @@ void MemViewer::renderMemory(QPainter& painter, const int rowIndex, const int co
   QColor oldPenColor = painter.pen().color();
   QColor fgColor = QGuiApplication::palette().color(QPalette::WindowText);
   int posXHex = m_rowHeaderWidth + (m_charWidthEm * 2 + m_charWidthEm / 2) * columnIndex;
-  if (!(m_currentFirstAddress + (m_numColumns * rowIndex + columnIndex) >= m_memViewStart &&
-        m_currentFirstAddress + (m_numColumns * rowIndex + columnIndex) < m_memViewEnd) ||
+  const bool validRange{
+      m_currentFirstAddress + (m_numColumns * rowIndex + columnIndex) >= m_memViewStart &&
+      m_currentFirstAddress + (m_numColumns * rowIndex + columnIndex) < m_memViewEnd};
+  if (!validRange ||
       !DolphinComm::DolphinAccessor::isValidConsoleAddress(
           m_currentFirstAddress + (m_numColumns * rowIndex + columnIndex)) ||
       !m_validMemory)

Source/GUI/MemWatcher/Dialogs/DlgImportCTFile.cpp
@@ -169,10 +169,7 @@ void DlgImportCTFile::accept()
     }
   }
 
-  if (m_btnGroupImportAddressMethod->checkedId() == 0)
-    m_useDolphinPointers = false;
-  else
-    m_useDolphinPointers = true;
+  m_useDolphinPointers = m_btnGroupImportAddressMethod->checkedId() != 0;
   m_strFileName = m_txbFileName->text();
 
   setResult(QDialog::Accepted);

commit f7e45d13d4684a12199dd49f2af4495766d6eed6

Avoid prematurely freeing buffer during value editing on Linux.
This was a regression in d58cd7687e272a576e49c, where the deletion of
the `bufferCopy` buffer was moved to an earlier point. It was overlooked
that the buffer is referenced by the `local` structure that used in the
`process_vm_writev()` call.

Source/DolphinProcess/Linux/LinuxDolphinProcess.cpp
@@ -273,9 +273,10 @@ bool LinuxDolphinProcess::writeToRAM(const u32 offset, const char* buffer, const
       break;
     }
   }
-  delete[] bufferCopy;
 
   const ssize_t nwrote{process_vm_writev(m_PID, &local, 1, &remote, 1, 0)};
+  delete[] bufferCopy;
+
   if (nwrote == -1)
   {
     // A more specific error type should be available in `errno` (if ever interested).