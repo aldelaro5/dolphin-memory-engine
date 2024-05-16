aldelaro5/dolphin-memory-engine

commit e8ea2b348697346c091ca2fb771a3d2bedf758e7

Set initial state of the **Scanner** checkable action in the next event loop tick.
Previously, the initial size of the main splitter was used to initialize
the checkable action. However, that size is not accurate until the main
window has been shown.

The action is now initialized in the next event loop tick, when the GUI
is ready.

Fixes #131.

Source/GUI/MainWindow.cpp
@@ -9,6 +9,7 @@
 #include <QMenuBar>
 #include <QShortcut>
 #include <QString>
+#include <QTimer>
 #include <QVBoxLayout>
 #include <string>
 
@@ -83,7 +84,10 @@ void MainWindow::makeMenus()
   m_actScanner = new QAction(tr("&Scanner"), this);
   m_actScanner->setShortcut(QKeySequence("F3"));
   m_actScanner->setCheckable(true);
-  m_actScanner->setChecked(m_splitter->sizes()[0] > 0);
+  QTimer::singleShot(0, [this]() {
+    QSignalBlocker signalBlocker(m_actScanner);
+    m_actScanner->setChecked(m_scanner->isVisible());
+  });
 
   m_actQuit = new QAction(tr("&Quit"), this);
   m_actAbout = new QAction(tr("&About"), this);

commit d31665a4d8fd41c19801868a29eb7fb466eaf57a

Use splitter's section size to determine whether scanner pane is visible.
This was a regression in e8ea2b3486973, where the visibility predicate
was changed to something more readable. As it turned out, the visibility
of the widget does not indicate whether it is visible: the splitter may
be collapsed.

The predicate has now been reverted to what it was previously.

Test plan:

- Launch DME.
- Collapse the scanner pane.
- Exit DME.
- Launch DME.

Without this change, the **View > Scanner** would be checked, even
though the pane is collapsed.

Source/GUI/MainWindow.cpp
@@ -86,7 +86,7 @@ void MainWindow::makeMenus()
   m_actScanner->setCheckable(true);
   QTimer::singleShot(0, [this]() {
     QSignalBlocker signalBlocker(m_actScanner);
-    m_actScanner->setChecked(m_scanner->isVisible());
+    m_actScanner->setChecked(m_splitter->sizes()[0] > 0);
   });
 
   m_actQuit = new QAction(tr("&Quit"), this);