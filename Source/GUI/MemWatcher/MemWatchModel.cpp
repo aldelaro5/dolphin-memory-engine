f5359e21778cc3dc2151c52b61dcfec50f1a652a
cristian64 <cristian64@gmail.com>
2024-05-17 21:50:56 +0100

    Replace use of `layoutChanged()` with more specific alternatives.
    `dataChanged()`, or `beginModelReset()` and `endModelReset()` are now
    used. Note that, whilst `layoutChanged()` can be used, it is generally
    used for layout changes, and not specific data changes or full model
    resets, and it generally needs a matching `layoutAboutToBeChanged()`
    call in advance.

--- Source/GUI/MemWatcher/MemWatchModel.cpp
+++ Source/GUI/MemWatcher/MemWatchModel.cpp
@@ -176,7 +176,7 @@ void MemWatchModel::editEntry(MemWatchEntry* entry, const QModelIndex& index)
 {
   MemWatchTreeNode* node = static_cast<MemWatchTreeNode*>(index.internalPointer());
   node->setEntry(entry);
-  emit layoutChanged();
+  emit dataChanged(index.siblingAtColumn(0), index.siblingAtColumn(columnCount({}) - 1));
 }
 
 void MemWatchModel::clearRoot()
@@ -690,8 +690,9 @@ void MemWatchModel::sortRecursive(int column, Qt::SortOrder order, MemWatchTreeN
 
 void MemWatchModel::loadRootFromJsonRecursive(const QJsonObject& json)
 {
+  beginResetModel();
   m_rootNode->readFromJson(json);
-  emit layoutChanged();
+  endResetModel();
 }
 
 MemWatchModel::CTParsingErrors MemWatchModel::importRootFromCTFile(QFile* const CTFile,
@@ -703,17 +704,13 @@ MemWatchModel::CTParsingErrors MemWatchModel::importRootFromCTFile(QFile* const 
   MemWatchTreeNode* importedRoot = parser.parseCTFile(CTFile, useDolphinPointer);
   if (importedRoot != nullptr)
   {
+    beginResetModel();
     delete m_rootNode;
     m_rootNode = importedRoot;
+    endResetModel();
   }
 
-  CTParsingErrors parsingErrors;
-  parsingErrors.errorStr = parser.getErrorMessages();
-  parsingErrors.isCritical = parser.hasACriticalErrorOccured();
-  if (!parsingErrors.isCritical)
-    emit layoutChanged();
-
-  return parsingErrors;
+  return {parser.getErrorMessages(), parser.hasACriticalErrorOccured()};
 }
 
 void MemWatchModel::writeRootToJsonRecursive(QJsonObject& json) const
