9aa9b27a4c1f540664df4bbbb7d2d2024d51a3cd
dreamsyntax <dreamsyntax@gmail.com>
2024-08-11 12:09:21 -0700

    Set version 1.2.3

--- Source/main.cpp
+++ Source/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
   QApplication::setApplicationName("Dolphin Memory Engine");
-  QApplication::setApplicationVersion("1.2.2");
+  QApplication::setApplicationVersion("1.2.3");
 
   SConfig config;  // Initialize global settings object
 
