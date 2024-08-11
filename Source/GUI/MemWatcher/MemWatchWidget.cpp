aldelaro5/dolphin-memory-engine

commit a7cb0856b05bf8977f0733406311cca2ece0acc6

Add options to copy pointer addresses (or base address) of a watch entry (#167)
* feat: Added right click menu to copy pointer address in Edit Address widget

Added a right click menu to m_pointerWidget which activates only if an address label is right clicked. It provides a single option (Copy address) which copies the address from that pointer level to clipboard.

Added an option (copy address) to the right click menu of a MemWatchWidget. Selecting Copy address on a pointer watch can copy the address at any pointer level to the clipboard. Selecting Copy address on a non-pointer watch will copy the base address of the watch to the clipboard.

Instead of finding the QLabel for the address when the pointer widget is clicked, the context menu is now registered to each pointer address label in the pointer widget. A property is also set (addr) which contains the numeric address at that pointer level. If addr is zero, the Copy Address is not enabled. Otherwise it is converted to a hex string which is copied to the clipboard if Copy Address is selected.

Source/GUI/MemWatcher/MemWatchWidget.cpp
@@ -273,6 +273,40 @@ void MemWatchWidget::onMemWatchContextMenuRequested(const QPoint& pos)
   connect(copy, &QAction::triggered, this, [this] { copySelectedWatchesToClipBoard(); });
   contextMenu->addAction(copy);
 
+  if (index.isValid())
+  {
+    MemWatchEntry* const entry = m_watchModel->getEntryFromIndex(index);
+    if (entry->isBoundToPointer())
+    {
+      QMenu* const copyAddrSubmenu = contextMenu->addMenu(tr("Copy add&ress..."));
+      QAction* const copyPointer = new QAction(tr("Copy &base address..."), this);
+      const QString addrString{QString::number(entry->getConsoleAddress(), 16).toUpper()};
+      connect(copyPointer, &QAction::triggered, this,
+              [addrString] { QApplication::clipboard()->setText(addrString); });
+      copyAddrSubmenu->addAction(copyPointer);
+      for (int i = 0; i < static_cast<int>(entry->getPointerLevel()); ++i)
+      {
+        if (!entry->getAddressForPointerLevel(i + 1))
+          break;
+        QAction* const copyAddrOfPointer =
+            new QAction(tr("Copy pointed address at &level %1...").arg(i + 1), this);
+        const QString addrString{
+            QString::number(entry->getAddressForPointerLevel(i + 1), 16).toUpper()};
+        connect(copyAddrOfPointer, &QAction::triggered, this,
+                [addrString] { QApplication::clipboard()->setText(addrString); });
+        copyAddrSubmenu->addAction(copyAddrOfPointer);
+      }
+    }
+    else
+    {
+      QAction* const copyPointer = new QAction(tr("Copy add&ress"), this);
+      const QString addrString{QString::number(entry->getConsoleAddress(), 16).toUpper()};
+      connect(copyPointer, &QAction::triggered, this,
+              [addrString] { QApplication::clipboard()->setText(addrString); });
+      contextMenu->addAction(copyPointer);
+    }
+  }
+
   QAction* paste = new QAction(tr("&Paste"), this);
   connect(paste, &QAction::triggered, this, [this, index] { pasteWatchFromClipBoard(index); });
   contextMenu->addAction(paste);

Source/GUI/MemWatcher/Dialogs/DlgAddWatchEntry.cpp
@@ -1,9 +1,12 @@
 #include "DlgAddWatchEntry.h"
 
+#include <QApplication>
+#include <QClipboard>
 #include <QDialogButtonBox>
 #include <QFontDatabase>
 #include <QFormLayout>
 #include <QHBoxLayout>
+#include <QMenu>
 #include <QMessageBox>
 #include <QVBoxLayout>
 #include <sstream>
@@ -167,6 +170,10 @@ void DlgAddWatchEntry::fillFields(MemWatchEntry* entry)
         QLabel* lblAddressOfPath = new QLabel();
         lblAddressOfPath->setText(
             QString::fromStdString(" -> " + m_entry->getAddressStringForPointerLevel(i + 1)));
+        lblAddressOfPath->setProperty("addr", m_entry->getAddressForPointerLevel(i + 1));
+        lblAddressOfPath->setContextMenuPolicy(Qt::CustomContextMenu);
+        connect(lblAddressOfPath, &QWidget::customContextMenuRequested, this,
+                &DlgAddWatchEntry::onPointerOffsetContextMenuRequested);
         m_addressPath.append(lblAddressOfPath);
         m_offsetsLayout->addWidget(lblLevel, i, 0);
         m_offsetsLayout->addWidget(txbOffset, i, 1);
@@ -190,6 +197,12 @@ void DlgAddWatchEntry::addPointerOffset()
   QLineEdit* txbOffset = new QLineEdit();
   m_offsets.append(txbOffset);
   QLabel* lblAddressOfPath = new QLabel(" -> ");
+  lblAddressOfPath->setText(
+      QString::fromStdString(" -> " + m_entry->getAddressStringForPointerLevel(level + 1)));
+  lblAddressOfPath->setProperty("addr", m_entry->getAddressForPointerLevel(level + 1));
+  lblAddressOfPath->setContextMenuPolicy(Qt::CustomContextMenu);
+  connect(lblAddressOfPath, &QWidget::customContextMenuRequested, this,
+          &DlgAddWatchEntry::onPointerOffsetContextMenuRequested);
   m_addressPath.append(lblAddressOfPath);
   m_offsetsLayout->addWidget(lblLevel, level, 0);
   m_offsetsLayout->addWidget(txbOffset, level, 1);
@@ -385,9 +398,10 @@ void DlgAddWatchEntry::updatePreview()
     for (int i = 0; i < level; ++i)
     {
       QLabel* lblAddressOfPath =
-          static_cast<QLabel*>(m_offsetsLayout->itemAtPosition(i, 2)->widget());
+          qobject_cast<QLabel*>(m_offsetsLayout->itemAtPosition(i, 2)->widget());
       lblAddressOfPath->setText(
           QString::fromStdString(" -> " + m_entry->getAddressStringForPointerLevel(i + 1)));
+      lblAddressOfPath->setProperty("addr", m_entry->getAddressForPointerLevel(i + 1));
     }
   }
 }
@@ -423,4 +437,24 @@ MemWatchEntry* DlgAddWatchEntry::stealEntry()
   MemWatchEntry* entry{m_entry};
   m_entry = nullptr;
   return entry;
+}
+
+void DlgAddWatchEntry::onPointerOffsetContextMenuRequested(const QPoint& pos)
+{
+  QLabel* const lbl = qobject_cast<QLabel*>(sender());
+
+  QMenu* const contextMenu = new QMenu(this);
+  QAction* const copyAddr = new QAction(tr("&Copy Address"), this);
+
+  const QString text{QString::number(lbl->property("addr").toUInt(), 16).toUpper()};
+  connect(copyAddr, &QAction::triggered, this,
+          [text] { QApplication::clipboard()->setText(text); });
+  contextMenu->addAction(copyAddr);
+
+  if (!lbl->property("addr").toUInt())
+  {
+    copyAddr->setEnabled(false);
+  }
+
+  contextMenu->popup(lbl->mapToGlobal(pos));
 }

Source/GUI/MemWatcher/Dialogs/DlgAddWatchEntry.h
@@ -44,6 +44,7 @@ private:
   void addPointerOffset();
   void removePointerOffset();
   void removeAllPointerOffset();
+  void onPointerOffsetContextMenuRequested(const QPoint& pos);
 
   MemWatchEntry* m_entry{};
   AddressInputWidget* m_txbAddress{};

commit e03abe85e991473c3cf7b24d2a16069d1df26fed

Check watch entry pointer to avoid dereferencing null pointer when a group node is right-clicked.
This was a regression in #167.

Group nodes do not have a watch entry associated with them. When a group
node was right-clicked, a null pointer would be fatally dereferenced:

```
#0  MemWatchEntry::isBoundToPointer() const (this=this@entry=0x0) at /w/dolphin-memory-engine/Source/MemoryWatch/MemWatchEntry.cpp:77
#1  0x0000561a4ab96c94 in MemWatchWidget::onMemWatchContextMenuRequested(QPoint const&) (this=0x561a4c28c580, pos=...) at /w/dolphin-memory-engine/Source/GUI/MemWatcher/MemWatchWidget.cpp:280
#2  0x00007f56ac7be023 in  () at /lib/x86_64-linux-gnu/libQt6Core.so.6
#3  0x00007f56ad489889 in QWidget::customContextMenuRequested(QPoint const&) () at /lib/x86_64-linux-gnu/libQt6Widgets.so.6
#4  0x00007f56ad4a6020 in QWidget::event(QEvent*) () at /lib/x86_64-linux-gnu/libQt6Widgets.so.6
#5  0x00007f56ad541406 in QFrame::event(QEvent*) () at /lib/x86_64-linux-gnu/libQt6Widgets.so.6
#6  0x00007f56ac765818 in QCoreApplicationPrivate::sendThroughObjectEventFilters(QObject*, QEvent*) () at /lib/x86_64-linux-gnu/libQt6Core.so.6
#7  0x00007f56ad44bd25 in QApplicationPrivate::notify_helper(QObject*, QEvent*) () at /lib/x86_64-linux-gnu/libQt6Widgets.so.6
#8  0x00007f56ad454c5e in QApplication::notify(QObject*, QEvent*) () at /lib/x86_64-linux-gnu/libQt6Widgets.so.6
#9  0x00007f56ac765a58 in QCoreApplication::notifyInternal2(QObject*, QEvent*) () at /lib/x86_64-linux-gnu/libQt6Core.so.6
#10 0x00007f56ad4b796c in  () at /lib/x86_64-linux-gnu/libQt6Widgets.so.6
#11 0x00007f56ad4ba635 in  () at /lib/x86_64-linux-gnu/libQt6Widgets.so.6
#12 0x00007f56ad44bd36 in QApplicationPrivate::notify_helper(QObject*, QEvent*) () at /lib/x86_64-linux-gnu/libQt6Widgets.so.6
#13 0x00007f56ac765a58 in QCoreApplication::notifyInternal2(QObject*, QEvent*) () at /lib/x86_64-linux-gnu/libQt6Core.so.6

Source/GUI/MemWatcher/MemWatchWidget.cpp
@@ -274,9 +274,9 @@ void MemWatchWidget::onMemWatchContextMenuRequested(const QPoint& pos)
   connect(copy, &QAction::triggered, this, [this] { copySelectedWatchesToClipBoard(); });
   contextMenu->addAction(copy);
 
-  if (index.isValid())
+  MemWatchEntry* const entry{MemWatchModel::getEntryFromIndex(index)};
+  if (entry)
   {
-    MemWatchEntry* const entry = m_watchModel->getEntryFromIndex(index);
     if (entry->isBoundToPointer())
     {
       QMenu* const copyAddrSubmenu = contextMenu->addMenu(tr("Copy add&ress..."));