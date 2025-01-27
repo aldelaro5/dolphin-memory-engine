7bc4bce982afe1bc3b62f2926171b632b3dfdb4b
jahorta <jahorta00000@gmail.com>
2025-01-27 15:24:37 -0600

    MemWatchWidget: For now, whenever a struct is expanded or collapsed, the node selection will be cleared.

--- Source/GUI/MemWatcher/MemWatchWidget.cpp
+++ Source/GUI/MemWatcher/MemWatchWidget.cpp
@@ -699,13 +699,17 @@ void MemWatchWidget::onRowsInserted(const QModelIndex& parent, const int first, 
   const QItemSelection selection{firstIndex,
                                  lastIndex.siblingAtColumn(MemWatchModel::WATCH_COL_NUM - 1)};
 
+
+  QItemSelectionModel* const selectionModel{m_watchView->selectionModel()};
   // If the parent node is a container and it is not expanded, do not select the child node or expand it.
   const MemWatchTreeNode* parentNode = MemWatchModel::getTreeNodeFromIndex(parent);
   if (parentNode != nullptr && parentNode->getEntry() != nullptr &&
-      GUICommon::isContainerType(parentNode->getEntry()->getType()) && !parentNode->isExpanded())
+      GUICommon::isContainerType(parentNode->getEntry()->getType()))
+  {
+    selectionModel->clearSelection();
     return;
+  }
 
-  QItemSelectionModel* const selectionModel{m_watchView->selectionModel()};
   selectionModel->select(selection, QItemSelectionModel::ClearAndSelect);
   selectionModel->setCurrentIndex(lastIndex, QItemSelectionModel::Current);
 
