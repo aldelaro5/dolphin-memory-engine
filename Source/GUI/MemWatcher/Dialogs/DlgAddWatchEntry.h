fedf7d98f2cb67d803d8fec764402b6f9b1233aa
jahorta <jahorta00000@gmail.com>
2025-01-27 07:03:35 -0600

    DlgAddWatchEntry: It should be showAddress not hideAddress as that is what is used in the cpp file. fixed.

--- Source/GUI/MemWatcher/Dialogs/DlgAddWatchEntry.h
+++ Source/GUI/MemWatcher/Dialogs/DlgAddWatchEntry.h
@@ -18,7 +18,7 @@ class DlgAddWatchEntry : public QDialog
 {
 public:
   DlgAddWatchEntry(bool newEntry, MemWatchEntry* entry, QVector<QString> structs, QWidget* parent,
-                   bool hideAddress = false);
+                   bool showAddress = true);
   ~DlgAddWatchEntry() override;
 
   DlgAddWatchEntry(const DlgAddWatchEntry&) = delete;
